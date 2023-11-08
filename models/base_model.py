56ea8ec7d340bef008db93fbf9995236b11db909
Success96 <lfawas6@gmail.com>
2023-11-08 12:42:04 +0100

    task 3 and task 4 successfully tested

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+#from models import storage
 
 class BaseModel:
 
@@ -12,10 +12,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initializes the  instance attributes"""
 
-
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
-                if key == "created_at":
+                if key == "__class__":
+                     pass
+                elif key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
                         kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
@@ -23,29 +24,26 @@ class BaseModel:
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
-      else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            #models.storage.new(self)
 
-    def _str_(self):
+    def __str__(self):
         """Returns string representation"""
-        return format(type(self). _class.name, self.id, self.dict_)
-
-
+        return f"[{self. __class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
             """Updates the public instance attribute updated_at with the current date and time."""
             self.updated_at = datetime.now()
 
-
     def to_dict(self):
-
             """returns a dictionary containing keys/values of _dict_"""
 
-            copy_dict = self._dict_.copy()
-            copy_dict["_class"] = type(self).class_
+            copy_dict = self.__dict__.copy()
+            copy_dict["__class__"] = self.__class__.__name__
             copy_dict["created_at"] = copy_dict["created_at"].isoformat()
             copy_dict["updated_at"] = copy_dict["updated_at"].isoformat()
             return copy_dict
+    
