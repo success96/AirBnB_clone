82de934cd22d524373b2168bd19adeec22806ca1
success96 <lfawas6@gmail.com>
2023-11-13 07:29:35 -0800

    pycodestyle

--- console.py
+++ console.py
@@ -13,10 +13,8 @@ from models.place import Place
 
 
 class HBNBCommand(cmd.Cmd):
-
     """Class for command interpreter."""
-
-    prompt= "(hbnb)"
+    prompt = "(hbnb)"
     __classes = [
             "BaseModel", "Amenity", "State", "City", "Review", "Place", "User"
             ]
@@ -31,11 +29,10 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         return
+
     def do_create(self, args):
-        """
-        This method Creates a new instance of BaseModel, 
-        saves it (to the JSON file) and prints the id
-        """
+        """This method Creates a new instance of BaseModel,
+            saves it (to the JSON file) and prints the id"""
         arg = args.split()
 
         if len(arg) == 0:
@@ -43,15 +40,13 @@ class HBNBCommand(cmd.Cmd):
         elif arg[0] not in self.__classes:
             print("** class doesn't exist **")
         else:
-            new_object = eval (f'{arg[0]}')()
+            new_object = eval(f'{arg[0]}')()
             print(new_object.id)
             models.storage.save()
 
     def do_show(self, args):
-        """
-        Prints the string representation of an instance 
-        based on the class name and id
-        """
+        """Prints the string representation of an instance
+            based on the class name and id"""
         arg = args.split()
 
         if len(arg) == 0:
@@ -67,47 +62,43 @@ class HBNBCommand(cmd.Cmd):
             print(models.storage.all()[obj_id])
 
     def do_destroy(self, args):
-            """
-            Deletes an instance based on the class name 
-            and id (save the change into the JSON file)
-            """
-            arg = args.split()
-
-            if len(arg) ==0:
-                print("** class name missing **")
-            elif arg[0] not in self.__classes:
-                print("** class doesn't exist **")
-            elif len(arg) == 1:
-                print("** instance id missing **")
-            elif f'{arg[0]}.{arg[1]}' not in models.storage.all():
-                print("** no instance found **")
-            else:
-                obj_id = f'{arg[0]}.{arg[1]}'
-                del models.storage.all()[obj_id]
-            models.storage.save()
-  
-    def do_all(self, args):
-        """
-        This method prints all string representation of all 
-        instances based or not on the class name
-        """
+        """Deletes an instance based on the class name
+            and id (save the change into the JSON file)"""
         arg = args.split()
 
-        if len(arg) ==0:
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif f'{arg[0]}.{arg[1]}' not in models.storage.all():
+            print("** no instance found **")
+        else:
+            obj_id = f'{arg[0]}.{arg[1]}'
+            del models.storage.all()[obj_id]
+        models.storage.save()
+
+    def do_all(self, args):
+        """This method prints all string representation of all
+            instances based or not on the class name"""
+        arg = args.split()
+
+        if len(arg) == 0:
             print([str(value) for value in models.storage.all().values()])
         elif arg[0] not in self.__classes:
-            print("** class doesn't exist **") 
+            print("** class doesn't exist **")
         else:
-            print([str(v) for k, v in models.storage.all().items() if k.startswith(arg[0])])
+            print([str(v) for k, v in models.storage.all().items()
+                  if k.startswith(arg[0])])
 
     def do_update(self, args):
-        """
-        This method updates an instance based on the class name 
-        and id by adding or updating attribute 
-        (save the change into the JSON file)
-        """
+        """This method updates an instance based on the class name
+            and id by adding or updating attribute
+            (save the change into the JSON file)"""
         arg = args.split()
-        if len(arg) ==0:
+
+        if len(arg) == 0:
             print("** class name missing **")
         elif arg[0] not in self.__classes:
             print("** class doesn't exist **")
@@ -125,66 +116,59 @@ class HBNBCommand(cmd.Cmd):
             att_value = arg[3]
             if att_value[0] == '"':
                 att_value = att_value[1:-1]
-            type_ = type(getattr(obj, att_name))           
+            type_ = type(getattr(obj, att_name))
             setattr(obj, att_name, type_(att_value))
             models.storage.save()
 
-    def default(self, args):
-        """This is the default behavior for cmd module when input is invalid"""
-        arg = args.split('.')
-        if arg[0] in self.__classes:
-            if arg[1]==  'all()':
-                self.do_all(arg[0])
-            elif arg[1]=='count()':
-                lst=[str(v) for k,v in models.storage.all().items()if k.startswith(arg[0])]
-                print(len(lst))
-<<<<<<< HEAD
-            elif arg[1].startswith("show"):
-                i_d = arg[1].split('"')[1]
-        
-=======
-            elif arg[1].startswith('show'):
-                i_d=arg[1].split('"')
-                obj_id=f'{arg[0]}.{i_d[1]}'
-                if obj_id not in models.storage.all():
-                    print('** no instance found **')
-                else:
-                    print(models.storage.all()[obj_id])
-            elif arg[1].startswith('destroy'):
-                i_d=arg[1].split('"')
-                obj_id=f"{arg[0]}.{i_d[1]}" 
-                if obj_id not in models.storage.all():
-                    print('** no instance found **') 
-                else: 
-                    del models.storage.all()[obj_id]
-                models.storage.save()
-            elif arg[1].startswith('update'):
-                att = arg[1].split('"')
-                obj_id=f"{arg[0]}.{att[1]}"
-                print(arg[1][47:-1]) 
-                if obj_id not in models.storage.all():
-                    print('** no instance found**')
-<<<<<<< HEAD
-                att_id= att[1]
-                att_name = att[3]
-                att_value = att[5]
-                lst = f'{arg[0]} {att_id} {att_name} {att_value}' 
-                self.do_update(lst)
->>>>>>> refs/remotes/origin/main
-=======
-                elif not (att[2].startswith(', {')):
-                        att_id= att[1]
+        def default(self, args):
+            """This is the default behavior for
+                cmd module when input is invalid"""
+            arg = args.split('.')
+
+            if arg[0] in self.__classes:
+                if arg[1] == 'all()':
+                    self.do_all(arg[0])
+                elif arg[1] == 'count()':
+                    lst = [str(v) for k, v in models.storage.all().items()
+                           if k.startswith(arg[0])]
+                    print(len(lst))
+                elif arg[1].startswith("show"):
+                    i_d = arg[1].split('"')[1]
+                elif arg[1].startswith('show'):
+                    i_d = arg[1].split('"')
+                    obj_id = f'{arg[0]}.{i_d[1]}'
+                    if obj_id not in models.storage.all():
+                        print('** no instance found **')
+                    else:
+                        print(models.storage.all()[obj_id])
+                elif arg[1].startswith('destroy'):
+                    i_d = arg[1].split('"')
+                    obj_id = f"{arg[0]}.{i_d[1]}"
+                    if obj_id not in models.storage.all():
+                        print('** no instance found **')
+                    else:
+                        del models.storage.all()[obj_id]
+                    models.storage.save()
+                elif arg[1].startswith('update'):
+                    att = arg[1].split('"')
+                    obj_id = f"{arg[0]}.{att[1]}"
+                    print(arg[1][47:-1])
+                    if obj_id not in models.storage.all():
+                        print('** no instance found**')
+                    elif not (att[2].startswith(', {')):
+                        att_id = att[1]
                         att_name = att[3]
                         att_value = att[5]
                         lst = f'{arg[0]} {att_id} {att_name} {att_value}'
                         self.do_update(lst)
-                else:
-                    att_dict = eval(arg[1][47:-1])
-                    obj = models.storage.all()[obj_id]
-                    for k, v in att_dict.items():
-                        type_ = type(getattr(obj, k)) 
-                        setattr(obj, k, type_(v))
-                        models.storage.save()
->>>>>>> 8ce22d9a69f39133b7db36d98e55c60c25215581
-if  __name__ == '__main__':
+                    else:
+                        att_dict = eval(arg[1][47:-1])
+                        obj = models.storage.all()[obj_id]
+                        for k, v in att_dict.items():
+                            type_ = type(getattr(obj, k))
+                            setattr(obj, k, type_(v))
+                            models.storage.save()
+
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
